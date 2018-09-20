cb36550feab60cc44d28e56590812e2fa169d7d4
Ana <anasantos1103@gmail.com>
2018-09-19 23:19:25 -0300

    Added routing to venda-module in dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VendaComponent } from './venda/venda/venda.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'venda', component: VendaComponent }
 ];
 
 @NgModule({
