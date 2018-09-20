37339c29a7ff0ca4d1dc90b888823c7e76ca5e6d
Ana <anasantos1103@gmail.com>
2018-09-19 23:43:23 -0300

    Created core.module with class produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { VendaModule } from './venda/venda.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { VendaModule } from './venda/venda.module';
     BrowserModule,
     DashboardModule,
     VendaModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/model/produto.ts
@@ -0,0 +1,9 @@
+export class Produto {
+    codigo : number;
+    nome : string;
+    preco : number = 0;
+    quantidadeEstoque : number;
+    codigoDeBarras : string;
+
+    constructor() {}
+}
