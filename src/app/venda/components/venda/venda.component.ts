19972ae26da3a80c5218c70b5fdc1e343be371f3
Ana <anasantos1103@gmail.com>
2018-09-20 00:30:15 -0300

    Added list of products in venda-component

--- src/app/venda/components/venda/venda.component.ts
+++ src/app/venda/components/venda/venda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from '../../../core/model/produto';
 
 @Component({
   selector: 'app-venda',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./venda.component.scss']
 })
 export class VendaComponent implements OnInit {
+  produtos: Array<Produto> = new Array<Produto>();
 
   constructor() { }
 
   ngOnInit() {
+    let p = new Produto();
+    let p2 = new Produto();
+
+    p.nome = "Cerveja Skol 350 ML";
+    p.codigo = 1;
+    p.codigoDeBarras = '7891149200504';
+    p.preco = 2.37;
+    p.quantidadeEstoque = 100;
+
+    p2.nome = "Cerveja Brahma 350 ML";
+    p2.codigo = 1;
+    p2.codigoDeBarras = '7891149010509';
+    p2.preco = 2;
+    p2.quantidadeEstoque = 80;
+
+    this.produtos.push(p);
+    this.produtos.push(p2);
   }
 
 }
