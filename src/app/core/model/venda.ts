595cc99562ed5ddaf5dde5bf605c01f793baf3a2
Ana <anasantos1103@gmail.com>
2018-09-23 18:33:10 -0300

    created total sale getter

--- src/app/core/model/venda.ts
+++ src/app/core/model/venda.ts
@@ -1,5 +1,6 @@
 import { Mesa } from './mesa';
 import { Pendura } from './pendura';
+import { ItemVenda } from './itemVenda';
 
 export class Venda {
   id: string;
@@ -7,7 +8,19 @@ export class Venda {
   finalizada?: boolean;
   quitada?: boolean;
   mesa?: Mesa;
-  itensVenda?: any[];
+  itensVenda?: ItemVenda[];
   penduras?: Pendura[];
   pagamento?: any[];
+  private _total: number;
+
+  get total() {
+    this._total = 0;
+    if (this.itensVenda != undefined) {
+      this.itensVenda.forEach(item => {
+        this._total += item.quantidade * item.valor;
+      })
+    }
+
+    return this._total
+  }
 }
