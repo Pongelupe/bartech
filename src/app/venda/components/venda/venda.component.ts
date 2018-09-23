fcdb5b1dffe04ff74e5e2f9aab183c43d9779960
Ana <anasantos1103@gmail.com>
2018-09-23 16:19:32 -0300

    Implemented product list search

--- src/app/venda/components/venda/venda.component.ts
+++ src/app/venda/components/venda/venda.component.ts
@@ -10,11 +10,13 @@ import { Venda } from '../../../core/model/venda';
 export class VendaComponent implements OnInit {
   produtos: Array<Produto> = new Array<Produto>();
   venda: Venda;
+  termoPesquisaProduto: string;
   constructor() { }
 
   ngOnInit() {
     let p = new Produto();
     let p2 = new Produto();
+    let p3 = new Produto();
 
     p.nome = "Cerveja Skol 350 ML";
     p.codigo = 1;
@@ -23,13 +25,20 @@ export class VendaComponent implements OnInit {
     p.quantidadeEstoque = 100;
 
     p2.nome = "Cerveja Brahma 350 ML";
-    p2.codigo = 1;
+    p2.codigo = 2;
     p2.codigoDeBarras = '7891149010509';
     p2.preco = 2;
     p2.quantidadeEstoque = 80;
 
+    p3.nome = "Cerveja Brahma Lata 350 ML";
+    p3.codigo = 3;
+    p3.codigoDeBarras = '7891149145789';
+    p3.preco = 20.58;
+    p3.quantidadeEstoque = 100;
+
     this.produtos.push(p);
     this.produtos.push(p2);
+    this.produtos.push(p3);
     this.venda = new Venda();
     this.venda.id = '1';
   }
