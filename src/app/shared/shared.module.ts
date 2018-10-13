5bde266185431c540c7cde80e4dff96aa06783f2
Ana <anasantos1103@gmail.com>
2018-10-13 17:30:45 -0300

    Retirado dependência sweetalert2 do projeto

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BtCardComponent } from './components/bt-card/bt-card.component';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
-import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
@@ -14,13 +13,11 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
     ReactiveFormsModule,
     BtCardComponent,
     NgxSmartModalModule,
-    SweetAlert2Module,
     AngularFontAwesomeModule
   ],
   imports: [
     CommonModule,
     NgxSmartModalModule.forRoot(),
-    SweetAlert2Module.forRoot()
   ],
   declarations: [BtCardComponent]
 })
