8be2c0cf429529aff0cbb3c8c362b99b63629431
Ana <anasantos1103@gmail.com>
2018-10-14 02:39:38 -0300

    Criado class utils com método para verificar se usuário está em dispositibo mobile.

---
+++ src/app/shared/utils.ts
@@ -0,0 +1,25 @@
+export class Utils {
+
+    public static isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i) && window.innerWidth < 768;
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i) && window.innerWidth < 768;
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i) && window.innerWidth < 768;
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i) && window.innerWidth < 768;
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i) && window.innerWidth < 768;
+        },
+        any: function () {
+            return (this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows());
+        }
+
+    };
+}
+
