823366e76a4cdc3add92fec10e3f68a6742074e4
Ana <anasantos1103@gmail.com>
2018-10-19 00:01:08 -0300

    Retirado toast de bem-vindo ao sistema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private toastrService: ToastrService) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.toastrService.info('Seja bem-vindo!');
-  }
+  ngOnInit() { }
 }
