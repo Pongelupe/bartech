1b56f33a87de38a59705706c4819952654a42f0e
Ana <anasantos1103@gmail.com>
2018-11-06 21:33:39 -0200

    Criado componente penduras-bebum para exibir todos os valores pendurados dos clientes.

---
+++ src/app/pagamento/components/pendura/penduras-bebum/penduras-bebum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-penduras-bebum',
+  templateUrl: './penduras-bebum.component.html',
+  styleUrls: ['./penduras-bebum.component.scss']
+})
+export class PendurasBebumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pagamento/pagamento.module.ts
+++ src/app/pagamento/pagamento.module.ts
@@ -3,12 +3,13 @@ import { SharedModule } from '../shared/shared.module';
 import { PagamentoRoutingModule } from './pagamento-routing.module';
 import { PagamentoComponent } from './components/pagamento/pagamento.component';
 import { BebumComponent } from './components/pendura/bebum/bebum.component';
+import { PendurasBebumComponent } from './components/pendura/penduras-bebum/penduras-bebum.component';
 
 @NgModule({
   imports: [
     SharedModule,
     PagamentoRoutingModule
   ],
-  declarations: [PagamentoComponent, BebumComponent]
+  declarations: [PagamentoComponent, BebumComponent, PendurasBebumComponent]
 })
 export class PagamentoModule { }
