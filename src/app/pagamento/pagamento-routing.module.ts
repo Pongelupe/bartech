ba943031a5230cf8ec848d7640ac27e49ee69bbc
Ana <anasantos1103@gmail.com>
2018-11-06 21:57:37 -0200

    Adicionado opção de rota no menu para visualizar os clientes pendurados.

--- src/app/pagamento/pagamento-routing.module.ts
+++ src/app/pagamento/pagamento-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagamentoComponent } from './components/pagamento/pagamento.component';
 import { PagamentoResolver } from './components/pagamento/pagamento.resolver';
+import { PendurasBebumComponent } from './components/pendura/penduras-bebum/penduras-bebum.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PagamentoComponent
+  },
+  {
+    path: 'pendurados',
+    component: PendurasBebumComponent
   },
   {
     path: ':id',
