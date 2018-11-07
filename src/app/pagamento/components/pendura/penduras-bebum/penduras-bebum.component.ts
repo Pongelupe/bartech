081b3092b8d270f4190b68d67a3c1b2d7f8b0f5a
Ana <anasantos1103@gmail.com>
2018-11-07 01:03:51 -0200

    Adicionado tabela para exibir todos os clientes com os valores de penduras.

--- src/app/pagamento/components/pendura/penduras-bebum/penduras-bebum.component.ts
+++ src/app/pagamento/components/pendura/penduras-bebum/penduras-bebum.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PagamentoService } from '../../../service/pagamento.service';
 import { Cliente } from '../../../../core/model/cliente';
+import { Observable } from 'rxjs';
+import { debug } from 'util';
+import { ArrayType } from '@angular/compiler';
 
 @Component({
   selector: 'app-penduras-bebum',
@@ -9,15 +12,34 @@ import { Cliente } from '../../../../core/model/cliente';
 })
 export class PendurasBebumComponent implements OnInit {
   pendurados: Cliente[];
+  totalPenduras: number;
+  totalPagamentosEmPenduras: number;
+  saldoPendura: number;
 
   constructor(private pagamentoService: PagamentoService) { }
 
   ngOnInit() {
     this.pagamentoService.getClientesPendurados().subscribe(res => {
-      this.pendurados = Object.assign(Cliente.prototype, res);
+      this.pendurados = res;
+      this.updateTotais();
     }, err => {
       console.log('Erro : ' + err);
     });
   }
 
+  updateTotais() {
+    this.totalPenduras = 0;
+    this.totalPagamentosEmPenduras = 0;
+
+    this.pendurados.forEach(cliente => {
+      cliente = Object.assign(Cliente.prototype, cliente);
+      if (cliente.penduras !== undefined && cliente.penduras.length > 0) {
+        cliente.penduras.forEach(item => {
+          this.totalPenduras += item.valor;
+          this.totalPagamentosEmPenduras += item.valorPagto;
+        });
+      }
+      this.saldoPendura = this.totalPenduras - this.totalPagamentosEmPenduras;
+    });
+  }
 }
