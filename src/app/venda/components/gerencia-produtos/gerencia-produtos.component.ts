9f551a492935442b74ab7b59c52c9cde833378b8
Ana <anasantos1103@gmail.com>
2018-11-10 17:56:45 -0200

    Adicionado botão de cadastrar novo produto para abrir modal.

--- src/app/venda/components/gerencia-produtos/gerencia-produtos.component.ts
+++ src/app/venda/components/gerencia-produtos/gerencia-produtos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VendaService } from '../../service/venda.service';
 import { Observable } from 'rxjs';
 import { Produto } from '../../../core/model/produto';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-gerencia-produtos',
@@ -12,10 +13,19 @@ export class GerenciaProdutosComponent implements OnInit {
   produtos$: Observable<Produto[]>;
   termoPesquisaProduto: string;
 
-  constructor(private vendaService: VendaService) { }
+  constructor(private vendaService: VendaService, private ngxSmartModalService: NgxSmartModalService) { }
 
   ngOnInit() {
     this.produtos$ = this.vendaService.getAllProdutos();
+  }
+
+  openModalProduto(): void {
+    this.Produto = new Produto();
+    this.ngxSmartModalService.getModal('produtoModal').open();
+  }
+
+  fecharModal(): void {
+    this.ngxSmartModalService.getModal('clienteModal').close();
   }
 
 }
