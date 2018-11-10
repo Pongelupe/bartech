042c8729e729bb8b945caa0161000d9c4ba51dd9
Ana <anasantos1103@gmail.com>
2018-11-10 18:41:40 -0200

    Criado componente produto.

--- src/app/venda/venda.module.ts
+++ src/app/venda/venda.module.ts
@@ -5,12 +5,13 @@ import { VendaRoutingModule } from './venda-routing.module';
 import { ProdutoPipe } from './pipes/produto.pipe';
 import { ItensVendaComponent } from './components/venda/itens-venda/itens-venda.component';
 import { GerenciaProdutosComponent } from './components/gerencia-produtos/gerencia-produtos.component';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 @NgModule({
   imports: [
     SharedModule,
     VendaRoutingModule
   ],
-  declarations: [VendaComponent, ProdutoPipe, ItensVendaComponent, GerenciaProdutosComponent]
+  declarations: [VendaComponent, ProdutoPipe, ItensVendaComponent, GerenciaProdutosComponent, ProdutoComponent]
 })
 export class VendaModule { }
